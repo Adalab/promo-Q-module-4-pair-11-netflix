3568ced932a1558e2ce4ba84e83c44fca57e340a
Laura Martin gonzalez <lauramartingonzalez@MacBook-Pro-de-Laura.local>
2022-09-05 11:16:34 +0200

    netflix express

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const movies = require('./data/movies.json');
+
 
 // create and config server
 const server = express();
@@ -11,3 +13,9 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+
+server.get('/movies', (req, resp) => {
+  const response = { "success": true, "movies": movies }
+  resp.json(response)
+})
