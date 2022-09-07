68b294e9bbbd7a59bb043eecd93a04242bcd2bf0
Laura Martin gonzalez <lauramartingonzalez@MacBook-Pro-de-Laura.local>
2022-09-07 11:28:50 +0200

    views ejs

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ const cors = require('cors');
 const movies = require('./data/movies.json');
 
 
+
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
+
+
+server.set('view engine', 'ejs');
 
 // init express aplication
 const serverPort = 4000;
@@ -19,6 +23,13 @@ server.get('/movies', (req, resp) => {
   const response = { "success": true, "movies": movies }
   resp.json(response)
 })
+
+server.get('/movie/:movieId', (req, res) => {
+  console.log(req.params.movieId);
+  const foundMovie = movies.find((oneMovie) => oneMovie.id === req.params.movieId);
+  console.log(foundMovie);
+  res.render('movie', foundMovie);
+});
 
 const staticServer = './src/public-react';
 server.use(express.static(staticServer));
