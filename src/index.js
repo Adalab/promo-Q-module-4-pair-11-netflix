afa1dc98619123f39e80d9169e33db5c0ee7ad9b
modehans <monicaglezsanch@gmail.com>
2022-09-09 11:42:16 +0200

    Install SQlite, import database, modify endpoint get movies and fetch to filter gender

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const movies = require('./data/movies.json');
-
-
+const Database = require('better-sqlite3');
 
 // create and config server
 const server = express();
 server.use(cors());
-server.use(express.json());
-
+server.use(express.json({ limit: '10mb' }));
 
 server.set('view engine', 'ejs');
+
+// confituración database
+const db = new Database('./src/db/database.db', {
+  verbose: console.log,
+});
 
 // init express aplication
 const serverPort = 4000;
@@ -18,15 +21,27 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-
 server.get('/movies', (req, resp) => {
-  const response = { "success": true, "movies": movies }
-  resp.json(response)
-})
+  if (req.query.gender === '') {
+    const query = db.prepare(`SELECT * FROM movies ORDER BY title`);
+    /*   const response = { success: true, movies: movies }; */
+    const response = query.all();
+    resp.json({ success: true, movies: response });
+    return;
+  } else {
+    const query = db.prepare(
+      `SELECT * FROM movies WHERE gender=? ORDER BY title`
+    );
+    const response = query.all(req.query.gender);
+    resp.json({ success: true, movies: response });
+    return;
+  }
+});
 
 server.get('/movie/:movieId', (req, res) => {
-  console.log(req.params.movieId);
-  const foundMovie = movies.find((oneMovie) => oneMovie.id === req.params.movieId);
+  const foundMovie = movies.find(
+    (oneMovie) => oneMovie.id === req.params.movieId
+  );
   console.log(foundMovie);
   res.render('movie', foundMovie);
 });
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,18 +1,17 @@
 // login
 
-const getMoviesFromApi = () => {
+const getMoviesFromApi = (params) => {
   console.log('Se están pidiendo las películas de la app');
 
-  return fetch('//localhost:4000/movies')
-    .then(response => response.json())
-    .then(data => {
+  return fetch(`//localhost:4000/movies?gender=${params.gender}`)
+    .then((response) => response.json())
+    .then((data) => {
       return data;
     });
-}
+};
 
 const objToExport = {
-  getMoviesFromApi: getMoviesFromApi
+  getMoviesFromApi: getMoviesFromApi,
 };
 
 export default objToExport;
-
